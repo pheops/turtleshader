7bbd79d3c4c32408a26b7c4c99d01ebee727a33a
pheops <92880990+pheops@users.noreply.github.com>
2022-01-06 20:48:59 -0800

    Update main.js

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-import * as THREE from "/node_modules/three/build/three.module.js";
+import * as THREE from "turtleshader/node_modules/three/build/three.module.js";
 import { OrbitControls } from "./node_modules/three/examples/jsm/controls/OrbitControls.js"; //'three/examples/jsm/controls/OrbitControls.js';
 import { DoubleDepthBuffer } from "./programs/doubleDepthBuffer.js";
 import { Blit } from "./programs/blit.js";
